f7a3f4e26e88b31a4415c727626d590a7c062a7d
mofumashu <eltolbo22@gmail.com>
2025-04-27 19:59:46 +0200

    modified:   server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -15,5 +16,5 @@ def erb():
     return render_template('erb.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    port = int(os.environ.get('PORT', 5000))  # ← this is important for Railway
+    app.run(host='0.0.0.0', port=port)
