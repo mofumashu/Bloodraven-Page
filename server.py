a53b6b5b6431e520c8b24e5582dcea906a578c57
mofumashu <eltolbo22@gmail.com>
2025-04-27 21:10:19 +0200

    modified:   server.py 	modified:   static/style.css 	modified:   templates/index.html

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -16,5 +15,5 @@ def erb():
     return render_template('erb.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    
